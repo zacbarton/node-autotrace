f688d300ad45d2f12d5109588b9d9ecb091d0281
zacbarton <zacbarton@gmail.com>
2014-08-06 21:18:32 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+autotrace
+========
+
+A simple interface for converting raster images into vector graphics using AutoTrace.
+
+More details about AutoTrace can be found [here][1].
+
+
+Requirements
+--------
+
+* [AutoTrace][1]
+
+
+Installation
+--------
+
+    $ npm install -g autotrace
+
+
+Output Formats
+--------
+Typical output formats include EPS, AI, SVG, DXF, PDF, MIF and Cairo. All supported output formats can be found by running;
+
+```sh
+autotrace --help
+```
+
+Basic Usage
+--------
+Convert a PNG to SVG.
+
+```javascript
+// input file, options and a callback
+autotrace('/path/to/image.png', {
+	outputFile: '/tmp/out.svg'
+}, function(err, buffer) {
+	if (!err) console.log('done');
+});
+
+// options and a callback
+autotrace({
+	inputFile: '/path/to/image.png'
+	, outputFile: '/tmp/out.eps'
+}, function(err, buffer) {
+	if (!err) console.log('done');
+});
+
+// chaining
+autotrace()
+	.inputFile('/path/to/image.png')
+	.outputFile('/tmp/out.pdf')
+	.exec(function(err, buffer) {
+		if (!err) console.log('done');
+	});
+```
+
+Streams
+--------
+```javascript
+// stream output to a WriteableStream
+autotrace('/path/to/image.png')
+	.outputFormat('svg')
+	.stream(function(err, stdout, stderr) {
+		var writeStream = fs.createWriteStream('/tmp/out.svg');
+  		stdout.pipe(writeStream);
+	});
+	
+// without a callback, .stream() returns a stream
+// this is just a convenience wrapper for above.
+var writeStream = fs.createWriteStream('/tmp/out.pdf');
+autotrace('/path/to/image.png', {outputFormat: 'pdf'})
+	.stream()
+	.pipe(writeStream);
+```
+
+API
+--------
+
+**autotrace(inputFile, options, callback)**
+
+The first argument can be either a file path or an options object. The only required option is inputFile (when not passing as the first argument); all others are optional. Refer to the above usage section for examples.
+
+**Options/Methods** - all of AutoTrace's are supported along with some custom ones.
+
+*AutoTrace*
+
+    # Refer to AutoTrace's help for details.
+    man autotrace
+
+ * backgroundColor
+ * centerline
+ * colorCount
+ * cornerAlwaysThreshold
+ * cornerSurround
+ * cornerThreshold
+ * despeckleLevel
+ * despeckleTightness
+ * dpi
+ * errorThreshold
+ * filterIterations
+ * inputFormat
+ * lineReversionThreshold
+ * lineThreshold
+ * log
+ * outputFile
+ * outputFormat
+ * preserveWidth
+ * removeAdjacentCorners
+ * tangentSurround
+ * reportProgress
+ * debugArch
+ * debugBitmap
+ * version
+ * widthWeightFactor
+
+*Custom*
+
+* inputFile - path to the input file.
+* binArgs - returns an array of the args to be passed to AutoTrace.
+* debugExec - returns the full exec + args as a string. Useful for debugging.
+
+**Methods**
+ * exec(callback) - start the tracing process. returns (err, buffer).
+ * stream - stream the results of the tracing. returns (err, stdout, stderr).
+
+If a callback (e.g. autotrace(file, callback)) is provided the exec method is automatically called for you.
+ 
+  [1]: http://autotrace.sourceforge.net/
---
+++ index.js
@@ -0,0 +1,219 @@
+var path = require('path');
+var spawn = require('child_process').spawn;
+var PassThrough = require('stream').PassThrough;
+
+module.exports = AutoTrace;
+
+function AutoTrace(inputFile, options, callback) {
+	if (!(this instanceof AutoTrace)) {
+		return new AutoTrace(inputFile, options, callback);
+	}
+	
+	this.args = [];
+	this.inputFile;
+	this.bin = 'autotrace';
+	
+	if (options && typeof options === 'object') {
+		options.inputFile = inputFile;
+	} else if (inputFile && typeof inputFile === 'object') {
+		callback = options;
+		options = inputFile;
+	} else if (typeof inputFile === 'string') {
+		callback = options;
+		options = {inputFile: inputFile};
+	}
+	
+	this.init(options);
+	
+	if (callback) {
+		return this.exec(callback);
+	}
+	
+	return this;
+}
+
+AutoTrace.prototype.init = function(options) {
+	for (var i in options) {
+		if (this[i] && typeof this[i] === 'function') {
+			this[i](options[i]);
+		}
+	}
+};
+
+AutoTrace.prototype.backgroundColor = function(color) {
+	this.args.push('-background-color', color);
+	return this;
+};
+
+AutoTrace.prototype.centerline = function(value) {
+	if (!!value) this.args.push('-centerline');
+	return this;
+};
+
+AutoTrace.prototype.colorCount = function(value) {
+	this.args.push('-color-count', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.cornerAlwaysThreshold = function(value) {
+	this.args.push('-corner-always-threshold', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.cornerSurround = function(value) {
+	this.args.push('-corner-surround', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.cornerThreshold = function(value) {
+	this.args.push('-corner-threshold', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.despeckleLevel = function(value) {
+	this.args.push('-despeckle-level', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.despeckleTightness = function(value) {
+	this.args.push('-despeckle-tightness', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.dpi = function(value) {
+	this.args.push('-dpi', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.errorThreshold = function(value) {
+	this.args.push('-error-threshold', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.filterIterations = function(value) {
+	this.args.push('-filter-iterations', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.inputFormat = function(value) {
+	this.args.push('-input-format', value);
+	return this;
+};
+
+AutoTrace.prototype.lineReversionThreshold = function(value) {
+	this.args.push('-line-reversion-threshold', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.lineThreshold = function(value) {
+	this.args.push('-line-threshold', parseFloat(value));
+	return this;
+};
+
+AutoTrace.prototype.log = function(value) {
+	if (!!value) this.args.push('-log');
+	return this;
+};
+
+AutoTrace.prototype.outputFile = function(file) {
+	this.args.push('-output-file', file);
+	return this;
+};
+
+AutoTrace.prototype.outputFormat = function(format) {
+	this.args.push('-output-format', format);
+	return this;
+};
+
+AutoTrace.prototype.preserveWidth = function(value) {
+	if (!!value) this.args.push('-preserve-width');
+	return this;
+};
+
+AutoTrace.prototype.removeAdjacentCorners = function(value) {
+	if (!!value) this.args.push('-remove-adjacent-corners');
+	return this;
+};
+
+AutoTrace.prototype.tangentSurround = function(value) {
+	this.args.push('-tangent-surround', parseInt(value));
+	return this;
+};
+
+AutoTrace.prototype.reportProgress = function(value) {
+	if (!!value) this.args.push('-report-progress');
+	return this;
+};
+
+AutoTrace.prototype.debugArch = function(value) {
+	if (!!value) this.args.push('-debug-arch');
+	return this;
+};
+
+AutoTrace.prototype.debugBitmap = function(value) {
+	if (!!value) this.args.push('-debug-bitmap');
+	return this;
+};
+
+AutoTrace.prototype.version = function(value) {
+	if (!!value) this.args.push('-version');
+	return this;
+};
+
+AutoTrace.prototype.widthWeightFactor = function(value) {
+	this.args.push('-width-weight-factor', parseFloat(value));
+	return this;
+};
+
+/* custom methods */
+AutoTrace.prototype.inputFile = function(file) {
+	this.inputFile = file;
+	return this;
+};
+
+AutoTrace.prototype.binArgs = function() {
+	return [].concat(this.args, this.inputFile);
+};
+
+AutoTrace.prototype.debugExec = function(value) {
+	if (!!value) console.log(this.bin, this.binArgs().join(' '));
+	return this;
+};
+
+AutoTrace.prototype.exec = function(callback) {
+	var err = '';
+	var buffer = '';
+	var trace = spawn(this.bin, this.binArgs());
+	
+	trace.stdout.on('data', function(data) {
+		buffer += data;
+	});
+	
+	trace.stderr.on('data', function(data) {
+		err += data;
+	});
+	
+	trace.stdout.on('close', function(code) {
+		if (callback) {
+			callback(err || code, buffer ? new Buffer(buffer) : null);
+		}
+	});
+};
+
+AutoTrace.prototype.stream = function(callback) {
+	var throughStream;
+	
+	if (typeof callback !== 'function') {
+		throughStream = new PassThrough();
+		
+		callback = function(err, stdout, stderr) {
+			stdout.pipe(throughStream);
+		};
+	}
+	
+	var trace = spawn(this.bin, this.binArgs());
+	
+	callback.call(this, null, trace.stdout, trace.stderr);
+	
+	return throughStream || this;
+};
